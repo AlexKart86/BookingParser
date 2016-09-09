902696c8712615940ac4f9c6b765890e910d5d3c
alex_kart <alex_kart@mail.ru>
2016-09-09 17:09:13 +0300

    *

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var parser = require('./parser');
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 
 var server =  http.createServer(function(req, res){
   switch (req.url) {
       case '/':
           sendFile(res, 'html/index.html');
+          break;
+      default :
+          try {
+              var file_full_path = path.resolve(__dirname, '.'+req.url);
+              var stats = fs.statSync(file_full_path);
+              if (stats.isFile()){
+                  sendFile(res, file_full_path);
+              }
+          }
+              catch (err){
+                  res.statusCode = 400;
+                  res.body = err.message;
+                  console.error(err.message);
+                  res.end();
+          }
   }
 });
 
