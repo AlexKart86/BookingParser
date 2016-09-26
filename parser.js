a38c454642679c2de20444df64d950a474d775f0
alex_kart <alex_kart@mail.ru>
2016-09-26 17:27:05 +0300

    *

--- parser.js
+++ parser.js
@@ -17,7 +17,31 @@ function BookingAnswerError(message){
 util.inherits(BookingAnswerError, Error);
 BookingAnswerError.prototype.name = "BookingAnswerError";
 
+function PropertyError(property){
+    this.property = property;
+    this.message = "Отсутствует поле "+property;
+}
 
+util.inherits(PropertyError, Error);
+PropertyError.prototype.name = "PropertyError";
+
+function check_property(obj, prop){
+    if (!obj.hasOwnProperty(prop))
+        throw new PropertyError(prop);
+}
+
+function copy_prop_with_check(source, dest, prop_name){
+    check_property(source, prop_name);
+    source[prop_name] = dest[prop_name];
+}
+
+//Создает объект "Поезд" по данным, пришедшим от УЗ
+function Train(json){
+  copy_prop_with_check(json, this, "num");
+  copy_prop_with_check(json, this, "model");
+  copy_prop_with_check(json, this, "category");
+    
+}
 
 //Парсит токен из хтмл текста
 function parse_token_from_html(content){
@@ -103,6 +127,21 @@ function find_trains(station_id_from, station_id_to, date_dep, token,  callback)
            callback(error, null);
     });
 }
+
+
+//find trains with seats detail
+function find_trains_ext(station_id_from, station_id_to, date_dep, token, callback){
+    find_trains(station_id_from, station_id_to, date_dep, token, function (err, data) {
+       if (err) {
+           callback(err, null);
+           return;
+       }
+
+
+
+    });
+}
+
 
 function ask_station_list(keywords, callback){
     var options = {
